1a487b26c40afd82edb84760a85bc21ad06b38d6
Mans Nilsson <intetaget>
2015-11-04 15:18:59 -0500

    Turned on B'crypt & added validations to user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  #before_validation :ensure_access_token!
+  validates_presence_of :username
+  validates_uniqueness_of :email, :username
+  validates_format_of :email, with: /.+@.+\..+/
+  validates :access_key, presence: true, uniqueness: true
+
+  # def ensure_access_token!
+  #   if self.access_token.blank?
+  #     self.access_token = User.generate_token
+  #   end
+  # end
+
+  # def self.generate_token
+  #   token = SecureRandom.hex
+  #   while User.exists?(access_token: token)
+  #     token = SecureRandom.hex
+  #   end
+  #   token
+  # end
+end
 end
