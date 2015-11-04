5596b7dcbafeb3318d4bb6cfd50b73e299127af7
violetaria <terri.chu@outlook.com>
2015-11-04 16:38:15 -0500

    add authenticate and current_user functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  def current_user
+    key = request.headers['Access-Key']
+    key && User.find_by(access_key: key)
+  end
+
+  def authenticate_user!
+    unless current_user
+      key = request.headers['Access-Key']
+      render json: { error: "Could not authenticate with access key:'#{key}'" },
+             status: :unauthorized
+    end
+  end
 end
