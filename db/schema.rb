126e733b4571def8355ac8d96fbf80b27b4b6534
Mans Nilsson <intetaget>
2015-11-05 10:45:19 -0500

    first card model & routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104195805) do
+ActiveRecord::Schema.define(version: 20151105153827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "front",      null: false
+    t.string   "back",       null: false
+    t.integer  "deck_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",        null: false
