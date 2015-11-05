e4e91bdeffecf8d9ecfd109afa25ee4ac3757b4d
Mans Nilsson <intetaget>
2015-11-05 16:56:21 -0500

    WE HAVE FLAMING CARDS

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,39 @@
+class CardsController < ApplicationController
+  before_action :authenticate_user!
+
+
+
+  def create
+    @card = Card.new(front: params[:front], back: params[:back], deck_id: params[:id])
+    #binding.pry
+    if @card.save
+      render 'create.json.jbuilder', status: :created 
+      else render json: {errors: @card.errors.full_messages},
+        status: :unproccessable_entity
+      end  
+    
+    
+
+
+
+    # render json: { id: card.id, front: card.front, back: card.back, deck_id: card.deck_id}
+    #redirect_to cards_path(card.link_id)
+  end
+
+  # def edit
+  #   card = Card.find(params[:id])
+  #   render :edit
+  # end
+
+  # def update
+  #   card = Card.find(params[:id])
+  #   #card.update(front:, back: params[:front],[:back] )
+  #   redirect_to cards_path(card.link_id)
+  # end
+
+  # def destroy
+  #   card = Card.find(params[:id])
+  #   card.destroy
+  #   redirect_to cards_path
+  # end
+end
---
+++ app/views/cards/create.json.jbuilder
@@ -0,0 +1,6 @@
+json.card do
+  json.id @card.id
+  json.front @card.front
+  json.back @card.back
+  json.deck_id @card.deck_id
+end
