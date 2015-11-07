62f657aacf1d22f75c761268922803fbc5d72844
violetaria <terri.chu@outlook.com>
2015-11-07 16:44:47 -0500

    migration and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106145044) do
+ActiveRecord::Schema.define(version: 20151107214329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20151106145044) do
     t.integer  "user_id",    null: false
     t.integer  "duration",   null: false
     t.boolean  "correct",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stars", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "deck_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
