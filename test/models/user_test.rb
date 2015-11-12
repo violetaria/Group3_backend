702b2a0532ed533fdcfb4104620794cd8b8eff56
violetaria <terri.chu@outlook.com>
2015-11-12 14:37:47 -0500

    updated user fixtures and user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,67 +6,85 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  def test_new_valid_user
+  test "can create valid user" do
     user = User.new(username: "user3",
                 fullname: "user three",
                 email: "user@three.com",
                 password: "userthreepassword")
 
-    assert user.valid?
+    assert user.save
+
+    assert user.errors.blank?
   end
 
-  def test_new_user_no_username
+  test "cannot create user with blank username" do
     user = User.new(fullname: "user three",
                     email: "user@three.com",
                     password: "userthreepassword")
 
-    refute user.valid?
+    refute user.save
+
+    assert user.errors.present?
   end
 
-  def test_new_user_invalid_username
-    user = User.new(username: "user1",
-                     fullname: "user",
-                     email: "user@user.com",
+  test "cannot create two users with same username" do
+    user1 = User.new(username: "terri",
+                     fullname: "terri",
+                     email: "test@test.com",
                      password: "userpassword")
-    refute user.valid?
+    user2 = User.new(username: "terri",
+                     fullname: "chu",
+                     email: "chu@test.com",
+                     password: "userpassword")
+
+    assert_equal user1.username,user2.username
+
+    assert user1.save
+    refute user2.save
+
+    assert user2.errors.present?
   end
 
-  def test_new_user_no_fullname
+  test "cannot create user with blank fullname" do
     user = User.new(username: "user3",
                     email: "user@three.com",
                     password: "userthreepassword")
 
-    refute user.valid?
-  end
+  refute user.save
 
-  def test_new_user_no_email
+  assert user.errors.present?
+end
+
+  test "cannot create user with blank email" do
     user = User.new(username: "user3",
                     fullname: "user three",
                     password: "userthreepassword")
 
-    refute user.valid?
+    refute user.save
+
+    assert user.errors.present?
   end
 
-  def test_new_user_invalid_email
-     user = User.new(username: "user3",
+  test "cannot create user with invalid email" do
+     user1 = User.new(username: "user3",
                     fullname: "user three",
                     email: "@three.com",
                     password: "userthreepassword")
 
-    refute user.valid?
+     refute user1.save
 
-    user = User.new(username: "user3",
-                    fullname: "user three",
-                    email: "user@one.com",
-                    password: "userthreepassword")
-     refute user.valid?
+     assert user1.errors.present?
+
   end
 
-  def test_new_user_no_password
+  test "cannot create user with blank password" do
     user = User.new(username: "user3",
                     fullname: "user three",
                     email: "user@three.com")
-    refute user.valid?
+    refute user.save
+
+    assert user.errors.present?
+
   end
 
 
