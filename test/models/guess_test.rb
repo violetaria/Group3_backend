913ec2d150b74ba376fa9aefd17e6243d05b0fcd
violetaria <terri.chu@outlook.com>
2015-11-13 14:44:43 -0500

    model tests for guess added

--- test/models/guess_test.rb
+++ test/models/guess_test.rb
@@ -4,4 +4,56 @@ class GuessTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "can create valid guess" do
+    guess = Guess.new(user_id: users(:one).id,
+                      card_id: cards(:one).id,
+                      duration: 5,
+                      correct: false)
+
+    assert guess.save
+
+    assert guess.errors.blank?
+  end
+
+  test "cannot create guess with blank user" do
+    guess = Guess.new(card_id: cards(:one).id,
+                      duration: 5,
+                      correct: false)
+
+    refute guess.save
+
+    assert guess.errors.present?
+  end
+
+  test "cannot create guess with blank card" do
+    guess = Guess.new(user_id: users(:one).id,
+                      duration: 5,
+                      correct: false)
+
+    refute guess.save
+
+    assert guess.errors.present?
+  end
+
+  test "cannot create guess with blank duration" do
+    guess = Guess.new(card_id: cards(:one).id,
+                      user_id: users(:one).id,
+                      correct: false)
+
+    refute guess.save
+
+    assert guess.errors.present?
+  end
+
+  test "cannot create guess with blank correct" do
+    guess = Guess.new(card_id: cards(:one).id,
+                      duration: 5,
+                      user_id: users(:one).id)
+
+    refute guess.save
+
+    assert guess.errors.present?
+  end
+
 end
